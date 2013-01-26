3e64ffb067760785f50ffea9b364c12e1140f2fc
sukesan1984 <takamikousuke@gmail.com>
2013-01-26 21:41:19 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoShare::Application.config.secret_token = '060fd728ca363e22862d0f6525b602010cfe5d3debe016c04dc2e3e7e52e65d29800941ca8a6970969e6dd1d2beb90727a822a119527b87454055069cd54e2e1'
